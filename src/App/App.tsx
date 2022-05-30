f4b640972d83c8211278bfe0df91ab055e95466a
Manol Donev <manol.donev@gmail.com>
2022-05-30 19:25:00 +0300

    chore: setup commitlint

---
+++ src/App/App.tsx
@@ -0,0 +1,10 @@
+import type { Component } from 'solid-js';
+
+const App: Component = () => {
+  return (
+    <div>Todo App      
+    </div>
+  );
+};
+
+export { App };
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite';
 import solidPlugin from 'vite-plugin-solid';
+import { undestructurePlugin } from "babel-plugin-solid-undestructure"
 
 export default defineConfig({
-  plugins: [solidPlugin()],
+  plugins: [...undestructurePlugin("ts"), solidPlugin()],
   build: {
     target: 'esnext',
     polyfillDynamicImport: false,
