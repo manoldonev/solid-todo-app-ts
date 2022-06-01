d79e0e5b6927975fa47580bc5ecaa8a6466f74d4
Manol Donev <manol.donev@gmail.com>
2022-06-01 12:27:34 +0300

    chore: setup tailwind (#6)

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,7 +1,7 @@
 import type { Component } from 'solid-js';
 
 const App: Component = () => {
-  return <div>Todo App</div>;
+  return <div class="text-3xl font-bold underline">Todo App</div>;
 };
 
 export { App };
