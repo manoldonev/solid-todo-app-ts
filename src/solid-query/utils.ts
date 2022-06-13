6df65e74c5ab9a563719101cd8c6c286549f3fd9
Manol Donev <manol.donev@gmail.com>
2022-06-13 16:24:51 +0300

    feat: add mutation support to solid-query (#18)

--- src/solid-query/utils.ts
+++ src/solid-query/utils.ts
@@ -1,4 +1,11 @@
-import type { QueryFunction, QueryKey, QueryOptions } from 'react-query/types';
+import type {
+  QueryFunction,
+  QueryKey,
+  QueryOptions,
+  MutationOptions,
+  MutationKey,
+  MutationFunction,
+} from 'react-query/types';
 
 export function isQueryKey(value: unknown): value is QueryKey {
   return typeof value === 'string' || Array.isArray(value);
@@ -18,4 +25,27 @@ export function parseQueryArgs<
   }
 
   return { ...arg2, queryKey: arg1 } as TOptions;
+}
+
+export function parseMutationArgs<TOptions extends MutationOptions<any, any, any, any>>(
+  arg1: MutationKey | MutationFunction<any, any> | TOptions,
+  arg2?: MutationFunction<any, any> | TOptions,
+  arg3?: TOptions,
+): TOptions {
+  if (isQueryKey(arg1)) {
+    if (typeof arg2 === 'function') {
+      return { ...arg3, mutationKey: arg1, mutationFn: arg2 } as TOptions;
+    }
+    return { ...arg2, mutationKey: arg1 } as TOptions;
+  }
+
+  if (typeof arg1 === 'function') {
+    return { ...arg2, mutationFn: arg1 } as TOptions;
+  }
+
+  return { ...arg1 };
+}
+
+export function noop(): undefined {
+  return undefined;
 }
