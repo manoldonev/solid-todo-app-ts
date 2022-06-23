5a79218530d307105468f92e500a8289ae6407c5
Manol Donev <manol.donev@gmail.com>
2022-06-23 20:22:17 +0300

    chore: fix merge commit

--- src/App/App.test.tsx
+++ src/App/App.test.tsx
@@ -4,7 +4,6 @@ import { render, screen, waitFor, within } from 'solid-testing-library';
 import { Router } from 'solid-app-router';
 import { QueryCache, QueryClient, setLogger } from 'react-query/core';
 import userEvent from '@testing-library/user-event';
-import { HopeProvider } from '@hope-ui/solid';
 import { QueryClientProvider } from '../solid-query';
 import { App } from './App';
 import { server } from '../mocks/msw/server';
@@ -38,9 +37,7 @@ const TestApp: Component = () => {
   return (
     <QueryClientProvider client={queryClient}>
       <Router base={import.meta.env.BASE_URL}>
-        <HopeProvider>
-          <App />
-        </HopeProvider>
+        <App />
       </Router>
     </QueryClientProvider>
   );
