22af8db14319334b526b46e027b80c623f7115f2
Manol Donev <manol.donev@gmail.com>
2022-06-29 12:28:21 +0300

    refactor: introduce modal form component

--- src/routes/taskNew/NewTaskModal/NewTaskModal.tsx
+++ src/routes/taskNew/NewTaskModal/NewTaskModal.tsx
@@ -1,24 +1,13 @@
 import type { Component } from 'solid-js';
-import { AddNewForm } from '../AddNewForm';
 import { Modal } from '../../../components/Modal';
 import { ModalHeader } from './ModalHeader';
+import { ModalForm } from './ModalForm';
 
 const NewTaskModal: Component<{ onClose?: () => void }> = (props) => {
-  let dialogElement: HTMLDialogElement;
-  const closeModal = (): void => {
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-call
-    dialogElement.close();
-  };
-
   return (
-    <Modal
-      ref={(element) => {
-        dialogElement = element;
-      }}
-      onClose={() => props.onClose?.()}
-    >
+    <Modal onClose={() => props.onClose?.()}>
       <ModalHeader title="Add New Item" />
-      <AddNewForm class="flex-grow md:flex-none px-8 py-8" onSubmit={closeModal} onCancel={closeModal} />
+      <ModalForm />
     </Modal>
   );
 };
