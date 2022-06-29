a6a3b92aac89923db40a3c26ce4dde5aade33061
Manol Donev <manol.donev@gmail.com>
2022-06-29 16:17:21 +0300

    Updates

---
+++ assets/index.7dfe4ddd.js
@@ -0,0 +1,1 @@
+import{B as t}from"./index.f365db0b.js";const a=t('<div data-testid="analytics" class="min-h-screen bg-background p-4 text-on-background transition-colors"><p>Analytics</p></div>',4);var o=()=>a.cloneNode(!0);export{o as Analytics};
